898aba8bddce3a0c82fe3db8133de8a0795a3200
Noor Albayaty <noor.albayaty90@gmail.com>
2021-01-16 21:35:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,3 +26,19 @@ function add(a,b)
 {
   return a+b;
 }
+function subtract(a,b){
+  return a-b;
+}
+ function multiply(a,b){
+   return a*b;
+ }
+ function divide(a,b){
+   return a/b;
+ }
+ function  increment(x){
+   return x++;
+ }
+ function decrement(x){
+   return x--;
+ }
+ 
